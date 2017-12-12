5705e866a85de966da2a5ea08eb2ef68f1f27bb1
sturgilltyffani <sturgilltyffani@gmail.com>
2017-12-12 11:51:02 -0600

    api calls all pass

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ const app = express();
 const apiKey = 'RrgYj2_KwycE8D9-H3B6P1QuWGLSy1Cloc5RtWwVzvl_pt0_4QUO8VJv7y1KjItWOmQccm9_ZaWqwislMaZkxCJiIQQEr5FXsYK3A3uBaw_l-5so2fOLyoF2PbguWnYx';
 app.use(bp.json());
 app.use(bp.urlencoded({ extended: true }));
+/****DISHES ****/
 // GET all dishes
 app.get('/db/dishes', (req, res, next) => {
     index_1.procedure("spGetDishes")
@@ -26,11 +27,12 @@ app.get('/db/dishes/:id', (req, res, next) => {
 });
 //POST a new dish 
 app.post("/db/dishes", (req, res, next) => {
-    index_1.procedure("spInsertDish", [req.body.name, req.body.ingredients, req.body.date, req.body.itsavaliable, req.body.rating])
+    index_1.procedure("spInsertDish", [req.body.name, req.body.rating, req.body.date, req.body.itsavaliable, req.body.ingredients])
         .then((dish) => {
         return res.json(dish);
     });
 });
+/****INGREDIENTS ****/
 //GET ingredients for one dish
 //POST new ingredients for a dish
 app.post("/db/ingredients", (req, res, next) => {
@@ -39,7 +41,8 @@ app.post("/db/ingredients", (req, res, next) => {
         return res.json(i);
     });
 });
-//  GET results from terms searched in Yelp API
+/****RESTAURANTS ****/
+//  GET results from terms searched in Yelp API for restaurants in Birmingham
 app.get('/db/restaurants', (req, res, next) => {
     axios.default.get(utils_1.formatYelpSearchUrl(req.query.term), {
         headers: {
@@ -48,6 +51,22 @@ app.get('/db/restaurants', (req, res, next) => {
     })
         .then((results) => {
         res.send(results.data);
+    });
+});
+//GET all restaurants (use this call when trying to get all or some saved restaurants)
+//DO NOT GET THIS ONE AND THE ABOVE CALL MIXED UP
+//P.S. IM NOT YELLING AT YOU JUST MAKING SURE YOU SEE
+app.get('/db/savedrestaurants', (req, res, next) => {
+    index_1.procedure("spGetRestaurants")
+        .then((rest) => {
+        res.json(rest[0]);
+    });
+});
+//GET one restaurant (use this call when trying to get saved restaurants)
+app.get('/db/restaurants/:id', (req, res, next) => {
+    index_1.procedure("spGetRestaurant", [+req.params.id])
+        .then((rest) => {
+        res.json(rest[0][0]);
     });
 });
 app.listen(process.env.PORT || 3000, () => {
--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,9 @@ const apiKey = 'RrgYj2_KwycE8D9-H3B6P1QuWGLSy1Cloc5RtWwVzvl_pt0_4QUO8VJv7y1KjItW
 app.use(bp.json())
 app.use(bp.urlencoded({extended: true}))
 
+
+/****DISHES ****/
+
 // GET all dishes
  app.get('/db/dishes', (req: express.Request, res: express.Response, next: express.NextFunction ) => {
     procedure("spGetDishes")
@@ -31,11 +34,13 @@ app.use(bp.urlencoded({extended: true}))
 
  //POST a new dish 
  app.post("/db/dishes", (req:express.Request, res:express.Response, next:express.NextFunction)=> {
-    procedure("spInsertDish",[req.body.name, req.body.ingredients, req.body.date, req.body.itsavaliable, req.body.rating])
+    procedure("spInsertDish",[req.body.name, req.body.rating, req.body.date, req.body.itsavaliable, req.body.ingredients])
     .then((dish) => {
         return res.json(dish)   
     })
 });
+
+/****INGREDIENTS ****/
 
 //GET ingredients for one dish
 
@@ -47,8 +52,10 @@ app.post("/db/ingredients", (req:express.Request, res:express.Response, next:exp
         return res.json(i)
     })
 });
+
+/****RESTAURANTS ****/
  
-//  GET results from terms searched in Yelp API
+//  GET results from terms searched in Yelp API for restaurants in Birmingham
  app.get('/db/restaurants', (req: express.Request, res: express.Response, next: express.NextFunction) => {
     axios.default.get(formatYelpSearchUrl(req.query.term), {
             headers: {
@@ -58,6 +65,24 @@ app.post("/db/ingredients", (req:express.Request, res:express.Response, next:exp
         .then((results) => {
             res.send(results.data);
         });
+ });
+
+ //GET all restaurants (use this call when trying to get all or some saved restaurants)
+ //DO NOT GET THIS ONE AND THE ABOVE CALL MIXED UP
+ //P.S. IM NOT YELLING AT YOU JUST MAKING SURE YOU SEE
+ app.get('/db/savedrestaurants', (req: express.Request, res: express.Response, next: express.NextFunction ) => {
+    procedure("spGetRestaurants")
+    .then((rest) => {
+        res.json(rest[0])
+    })
+ });
+
+ //GET one restaurant (use this call when trying to get saved restaurants)
+ app.get('/db/restaurants/:id', (req: express.Request, res: express.Response, next: express.NextFunction ) => {
+    procedure("spGetRestaurant", [+req.params.id])
+    .then((rest) => {
+        res.json(rest[0][0])
+    })
  });
 
 app.listen(process.env.PORT || 3000, () => {
