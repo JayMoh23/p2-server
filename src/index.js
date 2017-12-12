e9eb7170c4a712071dd59624f9e3060864485da6
Jeff Murray <Jeff@mbps-MacBook-Pro-2.local>
2017-12-12 10:32:24 -0600

    server axios work

--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express = require("express");
 const index_1 = require("./config/db/index");
 const app = express();
-app
-    .get('/*', (req, res) => {
-    index_1.procedure("spGetHotels")
-        .then((hotels) => {
-        res.json(hotels[0]);
+app.get('/*', (req, res) => {
+    index_1.procedure("spGetResturants")
+        .then((restaurants) => {
+        res.json(restaurants[0]);
+    });
+});
+app.get('/:id', (req, res) => {
+    index_1.procedure("spGetRestaurant")
+        .then((restaurants) => {
+        res.json(restaurants[1]);
     });
 });
 app.listen(process.env.PORT || 3000, () => {
